d48f7c1194f70b76f090c3b8a211eaa433283614
Nagah <E-n.Shinawy@lean.sa>
2021-10-28 10:46:57 +0300

    adding DictMixin

--- testdome/questions.py
+++ testdome/questions.py
@@ -9,6 +9,24 @@ def group_by_owners(files: dict) -> dict:
     return by_owners
 
 
+class DictMixin:
+    def __init__(self, container: dict):
+        self.container = container
+
+    def group_by_value(self):
+        by_value = dict()
+
+        for key, value in self.container.items():
+            if not by_value.get(value):
+                by_value[value] = []
+
+            by_value[value].append(key)
+        return by_value
+
+    def __len__(self):
+        return len(self.container.keys())
+
+
 def main():
     files = {
         "Input.txt": "Randy",
@@ -17,6 +35,9 @@ def main():
         "index.html": "Randy",
     }
     print(group_by_owners(files))
+    dic_helper = DictMixin(files)
+    print(dic_helper.group_by_value())
+    print(len(dic_helper))
 
 
 if __name__ == "__main__":
